734a9574af6e4557b3dc28bab85a98b668f11cea
Nathan Arnold <natearn@natearn.info>
2012-12-15 20:59:01 -0500

    mostly finished

--- main.rb
+++ main.rb
@@ -4,28 +4,43 @@ require './map.rb'
 include Rubygame
 
 max_res = Screen.get_resolution
-screen = Screen.open([640,480])
-screen.title = "You are the villain!"
+$screen = Screen.open([640,480])
+$screen.title = "You are the villain!"
 
 clock = Clock.new
 clock.calibrate
 clock.target_framerate = 30
 clock.enable_tick_events
 
-world = Map.new './world.bmp'
+world = Map.new
 
-world << City.new("Toronto", [30,40])
-world << City.new("London", [430,240])
+world << City.new("London", [30,40])
+world << City.new("Toronto", [220,220])
+world << City.new("Berlin", [250,360])
 world << City.new("Kingston", [43,340])
+world << City.new("Ottawa", [350,140])
+world << City.new("Montreal", [500,20])
 
 world["Toronto"].connect world["London"]
 world["Kingston"].connect world["London"]
+world["Toronto"].connect world["Ottawa"]
+world["Montreal"].connect world["Ottawa"]
+world["Toronto"].connect world["Berlin"]
+world["Kingston"].connect world["Berlin"]
 
-villain = Villain.new(world["London"])
-hero = Hero.new(world["Toronto"])
+$villain = Villain.new(world["London"])
+$hero = Hero.new(world["Toronto"])
 
 event_queue = EventQueue.new
 event_queue.enable_new_style_events
+
+def gameover surface, message
+	font = Rubygame::TTF.new "Share_Tech/ShareTech-Regular.ttf", 96
+	label = font.render_utf8(message,true,:black)
+	rect = label.make_rect
+	rect.center = surface.make_rect.center
+	label.blit surface, rect
+end
 
 catch :quit do
 	loop do
@@ -33,12 +48,20 @@ catch :quit do
 		event_queue.each do |event|
 			case event
 				when Events::QuitRequested then throw :quit
-				when Events::KeyboardEvent then break
+				when Events::KeyboardEvent 
+					$turn_over = $villain.handle event unless ($win || $lose)
 			end
 		end
-		world.draw screen
-		hero.draw screen
-		villain.draw screen
-		screen.flip
+		if $turn_over
+			$win = world.update
+			$lose = $hero.action unless $win
+			$turn_over = false
+		end
+		world.draw $screen
+		$hero.draw $screen
+		$villain.draw $screen
+		gameover $screen, "Villain Won" if $win
+		gameover $screen, "Villain Lost" if $lose
+		$screen.flip
 	end
 end
--- map.rb
+++ map.rb
@@ -1,11 +1,16 @@
 
 require 'rubygame'
+Rubygame::TTF.setup
+$name_font = Rubygame::TTF.new "Share_Tech/ShareTech-Regular.ttf", 18
+$funds_font = Rubygame::TTF.new "Share_Tech/ShareTech-Regular.ttf", 14
+
 
 class Map
 	include Rubygame::Sprites::Sprite
 
-	def initialize file, cities = {}
-		@image = Rubygame::Surface.load file
+	def initialize cities = {}
+		#@image = Rubygame::Surface.load file
+		@image = Rubygame::Surface.new([640,480]).fill(:blue)
 		@rect = @image.make_rect
 		@cities = cities
 	end
@@ -13,8 +18,20 @@ class Map
 	def draw surface
 		super(surface)
 		@cities.each_value { |city|
+			city.neighbours.each { |n|
+				surface.draw_line_a(city.position, n.position, :navy)
+			}
+		}
+		@cities.each_value { |city|
 			city.draw surface
 		}
+	end
+
+	def update
+		@cities.each_value { |city|
+			return true if city.update
+		}
+		return false
 	end
 
 	def [] name
@@ -31,19 +48,56 @@ class Map
 end
 
 class City
-	include Rubygame::Sprites::Sprite
+	attr_reader :name, :position, :radius, :neighbours, :priority, :last_checked
 
-	attr_reader :name, :rect, :neighbours
-
-	def image
-		@@image ||= Rubygame::Surface.load('./city.bmp')
+	def initialize name, position, radius = 32
+		@name = name
+		@position = position
+		@funds = rand(1000) + 500
+		@radius = radius
+		@neighbours = []
+		@name_label = $name_font.render_utf8(name,true,:yellow)
+		@priority = 1
+		@last_checked = 1
 	end
 
-	def initialize name, position
-		@name = name
-		@rect = self.image.make_rect
-		self.rect.topleft = position
-		@neighbours = []
+	def update
+		@funds += rand(200) + 50
+		@lab += 1 if @lab
+		@silo -= 1 if @silo
+		@priority += 1 if @priority <= @neighbours.size
+		@last_checked += 1
+		return (@silo && @silo <= 0)
+	end
+
+	def rob_bank
+		loss = @funds / 2
+		@funds -= loss
+		@priority = 0
+		return loss
+	end
+
+	def build_lab funds
+		return false if @lab
+		return false if funds < 2000
+		#build the lab
+		@lab = -3
+		return funds - 2000
+	end
+
+	def build_silo funds
+		return false if @silo
+		return false unless @lab && @lab >= 10
+		return false if funds < 10000
+		@lab -= 10
+		@silo = 10
+	end
+
+	def investigate
+		@silo = nil
+		@lab = nil
+		@last_checked = 0
+		return $villain.city == self
 	end
 
 	def connect city
@@ -51,6 +105,7 @@ class City
 		fail "cannot connect to self" if city.name == self.name
 		unless self.is_connected? city
 			@neighbours << city 
+			@priority = @neighbours.size
 			city.connect self # connections must be mutual
 		end
 	end
@@ -60,54 +115,157 @@ class City
 	end
 
 	def draw surface
-		@neighbours.each { |city|
-			surface.draw_line_a(self.rect.center, city.rect.center, :brown)
-		}
-		super(surface)
+		surface.draw_circle_s(@position, @radius, :green)
+		rect = @name_label.make_rect
+		pt = @position.dup
+		pt[1] += @radius
+		rect.midtop = pt
+		@name_label.blit surface, rect
+		@funds_label = $funds_font.render_utf8("$#{@funds}",true,:green)
+		pt = rect.midbottom
+		rect = @funds_label.make_rect
+		rect.midtop = pt
+		@funds_label.blit surface, rect
+		if @lab
+			@@lab_image ||= Rubygame::Surface.new([8,8]).fill(:white)
+			rect = @@lab_image.make_rect
+			rect.center = @position
+			rect[0] += @radius / 2
+			@@lab_image.blit surface, rect
+			@lab_label = $funds_font.render_utf8(@lab.to_s,true,:white)
+			pt = rect.midbottom
+			rect = @lab_label.make_rect
+			rect.midtop = pt
+			@lab_label.blit surface, rect
+		end
+		if @silo
+			@@silo_image ||= Rubygame::Surface.new([8,8]).fill(:brown)
+			rect = @@silo_image.make_rect
+			rect.center = @position
+			rect[0] -= @radius / 2
+			@@silo_image.blit surface, rect
+			@silo_label = $funds_font.render_utf8(@silo.to_s,true,:red)
+			pt = rect.midbottom
+			rect = @silo_label.make_rect
+			rect.midtop = pt
+			@silo_label.blit surface, rect
+		end
 	end
 
 	# cities can have stuff, including players (that might need to be drawn)
 end
 
-class Hero
-	include Rubygame::Sprites::Sprite
+class Villain
+	attr_reader :city
 
 	def initialize city
-		@location = city
+		@city = city
+		@target_index = 0
+		@funds = 1000
 	end
 
-	def image
-		@@image ||= Rubygame::Surface.load('./hero.bmp')
+	def draw surface
+		pt = @city.position.dup
+		pt[1] += (@city.radius / 2)
+		surface.draw_circle_s(pt,4,:yellow)
+		# draw target
+		pos = self.target.position
+		rad = self.target.radius * 1.25
+		surface.draw_circle_a(pos,rad,:yellow)
+		# draw funds
+		@funds_label = $name_font.render_utf8("$#{@funds}",true,:yellow)
+		rect = @funds_label.make_rect
+		rect.bottomleft = surface.make_rect.bottomleft
+		@funds_label.blit surface, rect
 	end
 
-	def rect
-		base = self.image.make_rect
-		base.center = @location.rect.center
-		base.move(0, (@location.rect.height / 4))
+	def move
+		@city = self.target
+		@target_index = 0
+	end
+
+	def target 
+		@city.neighbours[@target_index]
+	end
+
+	def move_target num = 0
+		@target_index = (@target_index + num) % @city.neighbours.size
+	end
+
+	def handle event
+		fail "wrong type" unless event.is_a? Rubygame::Events::KeyboardEvent
+		return if event.is_a? Rubygame::Events::KeyReleased
+		case event.key
+			when :right, :down
+				self.move_target 1
+				return false
+			when :left, :up
+				self.move_target -1
+				return false
+			when :space, :return
+				self.move
+				return true
+			when :r
+				@funds += @city.rob_bank
+				return true
+			when :l
+				change = @city.build_lab(@funds)
+				if change
+					@funds = change
+					return true
+				else
+					return false
+				end
+			when :s
+				change = @city.build_silo(@funds)
+				if change
+					@funds = change
+					return true
+				else
+					return false
+				end
+		end
 	end
 end
 
-class Villain
-	include Rubygame::Sprites::Sprite
+class Hero
 
 	def initialize city
-		@location = city
+		@city = city
 	end
 
-	def image
-		@@image ||= Rubygame::Surface.load('./villain.bmp')
-	end
+	def action
+		# common sense move
+		return @city.investigate if @city.priority == 1
 
-	def rect
-		base = self.image.make_rect
-		base.center = @location.rect.center
-		base.move(0, ((@location.rect.height / 4) * -1))
-	end
+		# sort by priority
+		options = @city.neighbours.sort { |a,b|
+			a.priority <=> b.priority
+		}
 
-	def move city
-		if @location.is_connected? city
-			@location = city
+		#randomly choose a move weighted on priority
+			# (1 / priority) chance of move
+			# priority caps at number of neighbours + 1
+		options.each { |town|
+			if rand(town.priority) == 0
+				@city = town
+				return false
+			end
+		}
+
+		# don't bother re-investigating cities without priority
+		if @city.last_checked < 2
+			@city = options.first
+			return false
 		end
+
+		# investigate
+		return @city.investigate
 	end
 
+	def draw surface
+		pt = @city.position.dup
+		pt[1] -= (@city.radius / 2)
+		surface.draw_circle_s(pt,4,:red)
+	end
 end
